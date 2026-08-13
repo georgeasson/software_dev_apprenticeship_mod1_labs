17d8851cb465f570c2e1f1a6730db9488b70bc45
George Asson <38807529+georgeasson@users.noreply.github.com>
2026-08-13 16:24:01 +0100

    Improvements and bug fixes
    - Fixed a bug in which a 'break' statement was not added in one of the cases.
    - Added an if statement at the end of the program to display an error message if incorrect input has been received.
    - Added whitespace to make code more readable
    - Added variables usd, eur, jpy to make code more readable
    - Simplified logic around calculating the amountAfterServiceFee, amountAfterTax, finalForeignAmount

--- AirportProgram.java
+++ AirportProgram.java
@@ -3,38 +3,45 @@ package lab2;
 public class AirportProgram {
 
 	public static void main(String[] args) {
+
 		double gbp = 4589.23;
+
 		// Currencies: 1= USD, 2=EUR, 3=JPY
-		int currency = 1;
+		int usd = 1;
+		int eur = 2;
+		int jpy = 3;
+
+		int currency = usd;
+
 		// Customer types: R = regular, V = VIP
 		String customerType = "R";
+
 		// Declare conversion rate, airport tax, service fee as 0 to initialise the values
 		double conversionRate = 0;
 		double airportTax = 0;
 		double serviceFee = 0;
+
 		// switch statement to provide logic for cases
 		switch (currency) {
-		case 1:
+		case usd:
 			conversionRate = 1.25;
 			break;
-		case 2:
+		case eur:
 			conversionRate = 1.15;
 			break;
-		case 3:
+		case jpy:
 			conversionRate = 180.50;
+			break;
 		default:
 			conversionRate = 0;
 		}
 		
+		// if statement to determine the service fee (GBP amounts >= £100 incur a 2.5% fee, < £100 incurs 5% fee)
 		if (gbp >= 100) {
 			serviceFee = 0.025;
-		} else {
+		} else if (gbp > 0) {
 			serviceFee = 0.05;
 		}
-		
-		// Calc amount after service fee
-		double amSF = (gbp * (1 - serviceFee));
-		System.out.println("Amount after service fee: " + amSF);
 		
 		// switch statement for customerType
 		switch (customerType) {
@@ -44,15 +51,26 @@ public class AirportProgram {
 		case "V":
 			airportTax = 0.05;
 			break;
+		default:
+			airportTax = 0;
 		}
+
+		// Calc amount after service fee
+		double amountAfterServiceFee = gbp * (1 - serviceFee);
 			
 		// Calc amount after tax
-		double amT = (gbp * (1 - airportTax));
-		System.out.println("Amount after tax: " + amT);
+		double amountAfterTax = amountAfterServiceFee * (1 - airportTax);
 		
 		// Calc final amount in foreign currency
-		double amFC = ((gbp * (1 - serviceFee)) * (1 - airportTax));
-		System.out.println("Final amount in foreign currency: " + amFC);
+		double finalForeignAmount = amountAfterTax * conversionRate;
+		
+		if (finalForeignAmount <= 0) {
+			System.out.println("Error.");
+		} else {		
+			System.out.printf("Final amount in foreign currency: %.2f%n", finalForeignAmount);
+		}
+
+
 	}
 
 }
